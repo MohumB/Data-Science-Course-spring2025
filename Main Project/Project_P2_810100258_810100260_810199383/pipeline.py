f3b4dc2d73d5b1cdf9949b197e1fac643e907b50
nelyasi71 <nelyasi71@gmail.com>
2025-05-10 13:11:51 +0330

    before upload version

--- Main Project/Project_P2_810100258_810100260_810199383/pipeline.py
+++ Main Project/Project_P2_810100258_810100260_810199383/pipeline.py
@@ -1,14 +1,26 @@
 import subprocess
+from pathlib import Path
 
 def run_pipeline():
+    script_dir = Path(__file__).parent
+    db_path = script_dir / "database" / "dataset.db"
+    text_csv = script_dir / "../../Phase 1/CSV/text.csv"
+    cleaned_text_csv = script_dir / "../../Phase 1/CSV/cleaned_text.csv"
+    sentences_csv = script_dir / '../../Phase 1/CSV/sentences.csv'
+
     print("Loading data...")
-    subprocess.run(["python", "scripts/load_data.py"])
+    subprocess.run(["python", "scripts/load_data.py",db_path,text_csv])
     
     print("Preprocessing data...")
-    subprocess.run(["python", "scripts/preprocess.py"])
+    subprocess.run(["python", "scripts/preprocess.py",text_csv,cleaned_text_csv])
     
     print("Feature Engineering...")
-    subprocess.run(["python", "scripts/feature_engineering.py"])
+    subprocess.run(["python", "scripts/feature_engineering.py",cleaned_text_csv,sentences_csv])
+
+    print("Importing sentences to database...")
+    subprocess.run(["python", "scripts/import_to_db.py",sentences_csv,db_path])
 
 if __name__ == "__main__":
     run_pipeline()
+
+
--- Main Project/Project_P2_810100258_810100260_810199383/scripts/load_data.py
+++ Main Project/Project_P2_810100258_810100260_810199383/scripts/load_data.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from database_connection import connect_db
+import sys
 
-def load_data():
-    db_path="../database/dataset.db"
+def load_data(db_path):
     conn = connect_db(db_path)
     
-    # should change it later
-    query = "SELECT token_index, token_text FROM tokens ORDER BY doc_id, token_index"
-    
+    query = "SELECT text FROM text"
     df = pd.read_sql(query, conn)
     conn.close()
     
     return df
 
 if __name__ == "__main__":
-    df = load_data()
-    df.to_csv("../../Phase 1/CSV/text.csv", index=False)
+    db_path = sys.argv[1]
+    text_csv = sys.argv[2]
+    df = load_data(db_path)
+    df.to_csv(text_csv, index=False)
